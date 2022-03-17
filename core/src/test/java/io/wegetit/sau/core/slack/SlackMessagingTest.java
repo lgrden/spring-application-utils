995cb77d88d99734c17aa8e8c2ebc8712199fe63
lgrden <lgrden@gmail.com>
2022-03-18 00:04:36 +0100

    cleanup

--- core/src/test/java/io/wegetit/sau/core/slack/SlackMessagingTest.java
+++ core/src/test/java/io/wegetit/sau/core/slack/SlackMessagingTest.java
@@ -32,7 +32,7 @@ public class SlackMessagingTest {
 
     @Test
     public void sendMessage() throws URISyntaxException, JsonProcessingException {
-        MockRestServiceServer mockServer = MockRestServiceServer.createServer(service.SLACK_REST_TEMPLATE);
+        MockRestServiceServer mockServer = MockRestServiceServer.createServer(SlackMessageService.SLACK_REST_TEMPLATE);
 
         SlackMessage message = SlackMessage.builder()
             .text("This is a slack test message")
@@ -48,7 +48,7 @@ public class SlackMessagingTest {
                 .andExpect(method(HttpMethod.POST))
                 .andRespond(withStatus(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(service.SLACK_OBJECT_MAPPER.writeValueAsString(message))
+                .body(SlackMessageService.SLACK_OBJECT_MAPPER.writeValueAsString(message))
             );
         service.send("This is a slack test message", SlackAttachment.of("Welcome Test", "red"));
         mockServer.verify();
